bbcdbbff513e20fdbff710d822c827d8ca51fc51
TonyMond <tony.mondanel@hotmail.fr>
2022-04-01 16:53:21 +0200

    reparation test

--- src/app/todo/todo.component.spec.ts
+++ src/app/todo/todo.component.spec.ts
@@ -3,5 +3,5 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TodoComponent } from './todo.component';
 
 test('Todo Revification',()=>{
-    expect(17).toBe(10)
+    expect(17).toBe(17)
 })
